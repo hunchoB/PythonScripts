3abdc447fe42ff4ed011e7294cf903ff89b412eb
hunchob <alex.bobkov98@mail.ru>
2021-08-25 17:36:18 +0300

    Fixed

--- parse_xlsx.py
+++ parse_xlsx.py
@@ -1,4 +1,19 @@
-import pandas as pd
+import openpyxl
 
-var = pd.read_excel(r'D:\vscodeProjects\Software.xlsx', sheet_name='OSI')
-print(var)
+pathToFile = "D:\\vscodeProjects\\Python\\Software.xlsx"
+
+wb_obj = openpyxl.load_workbook(pathToFile)
+# sheet_obj = wb_obj.active
+
+sheets = wb_obj.sheetnames
+
+for i in sheets:
+    ws_obj = wb_obj[i]
+    print(ws_obj.title , ws_obj.max_row)
+
+
+# max_col = sheet_obj.max_column
+
+# for i in range(1, max_col + 1):
+#     cell_obj = sheet_obj.cell(row = 1, column = i)
+#     print(cell_obj.value)
